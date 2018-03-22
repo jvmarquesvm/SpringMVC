42cfeef5c170b0e7c8dd4e1bb991a6f35403cc2b
Joao_Victor <Joao_Victor>
2018-03-21 23:40:22 -0300

    #2 - Alura - Curso SpringMVC2 - Módulo 2 - Finalizado

--- src/main/java/com/br/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/com/br/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -24,61 +25,66 @@ import com.br.casadocodigo.loja.models.TipoPreco;
 import com.br.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
-@RequestMapping(value="/produtos")
+@RequestMapping(value = "/produtos")
 public class ProdutosController {
-	
+
 	@Autowired
 	private ProdutoDao produtoDao;
-	
+
 	@Autowired
 	private FileSaver fileSaver;
-	
+
 	@InitBinder
-	public void initBinder(WebDataBinder bind){
+	public void initBinder(WebDataBinder bind) {
 		bind.addValidators(new ProdutoValidation());
 	}
 
 	@RequestMapping("/produtosform")
-	public ModelAndView produtosRequisicao(Produto produto){
+	public ModelAndView produtosRequisicao(Produto produto) {
 		ModelAndView mv = new ModelAndView("produtos/produtosform");
-		//tipo = id que será enviado para view
+		// tipo = id que será enviado para view
 		mv.addObject("tipos", TipoPreco.values());
 		return mv;
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	@CacheEvict(value="produtosHome", allEntries=true)
-	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, 
-			                        BindingResult resultado, RedirectAttributes reAttr ){
-		if(resultado.hasErrors()){
+
+	@RequestMapping(method = RequestMethod.POST)
+	@CacheEvict(value = "produtosHome", allEntries = true)
+	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto,
+			BindingResult resultado, RedirectAttributes reAttr) {
+		if (resultado.hasErrors()) {
 			return produtosRequisicao(produto);
 		}
 		System.out.println(sumario.getOriginalFilename());
-		String sumarioReal =  fileSaver.write("arquivos-sumario", sumario);	
+		String sumarioReal = fileSaver.write("arquivos-sumario", sumario);
 		produto.setSumarioPath(sumarioReal);
-		
+
 		produtoDao.gravar(produto);
-		
+
 		reAttr.addFlashAttribute("retorno", "Produto cadastrado com sucesso!");
 		return new ModelAndView("redirect:produtos");
 	}
-	
-	@RequestMapping( method=RequestMethod.GET)
-	public ModelAndView listar(){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView listar() {
 		List<Produto> produtos = produtoDao.listarProduto();
 		ModelAndView mv = new ModelAndView("produtos/produtosformlist");
 		mv.addObject("produtos", produtos);
 		return mv;
 	}
-	
-	//@RequestMapping("/detalhe")
+
+	// @RequestMapping("/detalhe")
 	@RequestMapping("/detalhe/{id}")
-	//public ModelAndView find(int id){
-	public ModelAndView find( @PathVariable("id") Integer id){
+	// public ModelAndView find(int id){
+	public ModelAndView find(@PathVariable("id") Integer id) {
 		ModelAndView mv = new ModelAndView("/produtos/detalhe");
 		Produto produto = produtoDao.procura(id);
 		mv.addObject("produto", produto);
 		return mv;
 	}
 
+	/*@RequestMapping("/{id}")
+	@ResponseBody
+	public Produto findJson(@PathVariable("id") Integer id) {
+		return produtoDao.procura(id);
+	}*/
 }
