d26788efdfbcff309ab04514d0998049d4f743e0
Joao_Victor <Joao_Victor>
2018-04-10 03:19:38 -0300

    #8 - Alura - Curso SpringMVC2 - Módulo 8 - Email

--- src/main/java/com/br/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/com/br/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -2,6 +2,7 @@ package com.br.casadocodigo.loja.conf;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -15,6 +16,8 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
@@ -137,5 +140,23 @@ public class AppWebConfiguration  extends WebMvcConfigurerAdapter {
     @Bean
     public LocaleResolver localeResolver(){
         return new CookieLocaleResolver();
+    }
+    
+    //Enviando Email
+    @Bean
+    public MailSender mailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setUsername("joaovictor2@gmail.com");
+        mailSender.setPassword("holysoldier");
+        mailSender.setPort(587);
+
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", true);
+        mailProperties.put("mail.smtp.starttls.enable", true);
+        
+        mailSender.setJavaMailProperties(mailProperties);
+        return mailSender;
     }
 }
--- src/main/java/com/br/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/com/br/casadocodigo/loja/controllers/PagamentoController.java
@@ -4,10 +4,13 @@ import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.HttpClientErrorException;
+//import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.br.casadocodigo.loja.models.CarrinhoCompras;
 import com.br.casadocodigo.loja.models.DadosPagamento;
+import com.br.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -25,15 +29,21 @@ public class PagamentoController {
 	CarrinhoCompras carrinhoCompras;
 	@Autowired
 	RestTemplate rest;
+	@Autowired
+	MailSender emailSender;
 	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
 	//public ModelAndView finalizar(RedirectAttributes model){
-	public Callable<ModelAndView> finalizar(RedirectAttributes model){
+	//public Callable<ModelAndView> finalizar(RedirectAttributes model){
+	public Callable<ModelAndView> finalizar( @AuthenticationPrincipal Usuario usuario, RedirectAttributes model){  // modificado para envio de email
 		return () -> {
 			try{
 				String url = "http://book-payment.herokuapp.com/payment";
 				String response = rest.postForObject(url, new DadosPagamento(carrinhoCompras.getTotal()), String.class);
 				System.out.println(carrinhoCompras.getTotal());
+				
+				enviaEmailCompraProduto(usuario);
+				
 				model.addFlashAttribute("retorno", response);
 				return new ModelAndView("redirect:/produtos");
 			} catch ( Exception e) {
@@ -42,6 +52,16 @@ public class PagamentoController {
 			        return new ModelAndView("redirect:/produtos");
 			}
 		};
+	}
+
+	private void enviaEmailCompraProduto(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setSubject("Compra Finalizada com sucesso!" );
+		email.setTo("joaovictor2@hotmail.com");
+		email.setText("Compra realizada no valor de R$" + carrinhoCompras.getTotal());
+		email.setFrom("joaovictor2@gmail.com");
+		
+		emailSender.send(email);
 	}
 }
 
